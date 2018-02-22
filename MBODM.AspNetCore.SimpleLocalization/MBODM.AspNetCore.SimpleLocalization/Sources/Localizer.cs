94bd8a8215f493b0b665bdca2959685941c0d3d8
Marcel Beck <marcel.beck@rothsoft.de>
2018-02-22 17:58:18 +0100

    First commit.

---
+++ MBODM.AspNetCore.SimpleLocalization/MBODM.AspNetCore.SimpleLocalization/Sources/Localizer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Localization;
+
+namespace MBODM.AspNetCore.SimpleLocalization
+{
+    public sealed class Localizer<T> : ILocalizer
+    {
+        private readonly IStringLocalizer<T> stringLocalizer;
+
+        public Localizer(IStringLocalizer<T> stringLocalizer)
+        {
+            this.stringLocalizer = stringLocalizer ?? throw new ArgumentNullException(nameof(stringLocalizer));
+        }
+
+        public string this[string key]
+        {
+            get { return GetText(key); }
+        }
+
+        public string GetText(string key)
+        {
+            return stringLocalizer[key];
+        }
+    }
+}
---
+++ MBODM.AspNetCore.SimpleLocalization/MBODM.AspNetCore.SimpleLocalization/Sources/SimpleLocalizationMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MBODM.AspNetCore.SimpleLocalization
+{
+    public static class SimpleLocalizationMiddleware
+    {
+        public static IServiceCollection AddSimpleLocalization<TSharedResourceClass>(
+            this IServiceCollection serviceCollection, string resourcesPath)
+            where TSharedResourceClass : class
+        {
+            return serviceCollection.
+                AddScoped<ILocalizer, Localizer<TSharedResourceClass>>().
+                AddLocalization(options => options.ResourcesPath = resourcesPath);
+        }
+
+        public static IServiceCollection AddSimpleLocalization<TSharedResourceClass>(
+            this IServiceCollection serviceCollection)
+            where TSharedResourceClass : class
+        {
+            return serviceCollection.AddSimpleLocalization<TSharedResourceClass>("Resources");
+        }
+
+        public static IMvcBuilder AddSimpleLocalizationForDataAnnotations<TSharedResourceClass>(
+            this IMvcBuilder mvcBuilder)
+            where TSharedResourceClass : class
+        {
+            return mvcBuilder.AddDataAnnotationsLocalization(options =>
+            {
+                options.DataAnnotationLocalizerProvider = (type, factory) =>
+                {
+                    return factory.Create(typeof(TSharedResourceClass));
+                };
+            });
+        }
+    }
+}
